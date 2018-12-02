79feb2cc25dd5030cd4c6b640f33aa44b85e94b8
Julien Milletre Akipe <code.julien@milletre.fr>
2018-12-02 02:29:57 +0100

    initial support with encryption support

---
+++ BoardConfig.mk
@@ -0,0 +1,70 @@
+#
+# Copyright (C) 2015-2017 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/motorola/cedric
+
+include device/motorola/msm8937-common/BoardConfigCommon.mk
+
+TARGET_ARCH := arm64
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+TARGET_BOARD_PLATFORM := msm8937
+
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
+TARGET_NO_BOOTLOADER := true
+
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216        # mmcblk0p37
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456      # mmcblk0p52
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616    # mmcblk0p38
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3623878656    # mmcblk0p53
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 10768858624 # mmcblk0p54
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_LZ4C_DT := true
+#TARGET_CUSTOM_DTBTOOL := dtbTool_custom
+BOARD_DTBTOOL_ARGS := --force-v3 --motorola 1
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := cedric_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8937
+
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
---
+++ README.md
@@ -0,0 +1,3 @@
+# Squid TWRP tree for Moto G5 (cedric)
+
+See the common README for more info.
