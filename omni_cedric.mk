12f82b2b44c5117e051dbab460ad1afdbbf7cdfb
Julien Milletre Akipe <code.julien@milletre.fr>
2019-03-24 13:01:13 +0100

    enable sdcardfs

--- omni_cedric.mk
+++ omni_cedric.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
+# sdcardfs
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.sdcardfs=true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE          := cedric
 PRODUCT_NAME            := omni_cedric
